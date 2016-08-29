bdf619cc8f30e4fe80e450d50cd37c0af715a40e
brianAround <blclark4@cox.net>
2016-08-29 16:16:58 -0500

    New Url, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -63,7 +63,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Frances visits the home page.  There is no sign of Edith's
         # list
-        slef.browser.get(self.live_server_url)
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -72,7 +72,7 @@ class NewVisitorTest(LiveServerTestCase):
         # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
-        inputbox.sned_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         # Frances gets her own unique URL
         frances_list_url = self.browser.current_url
