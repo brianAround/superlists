efd552c53cfadbeb6e3b93a24d27a7cc4e2a21a9
brianAround <blclark4@cox.net>
2016-08-30 14:00:39 -0500

    new URL + view for adding to existing lists. FT passes

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
-
-
+    list = models.ForeignKey(List, default=None)
 # Pick this up with "Our First Database Migration" from
 # URL: http://www.obeythetestinggoat.com/book/chapter_05.html
